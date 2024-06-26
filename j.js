544375bb2c1d03baf187f21ac59776f820651f13
ali <ali@gmail.com>
2024-06-26 13:13:34 +0200

    save

---
+++ j.js
@@ -0,0 +1,45 @@
+let Open = document.getElementById('open');
+
+let Close = document.getElementById('close');
+
+
+let list = document.getElementById('nav-list');
+
+
+Open.onclick =function x( ){
+
+list.classList.add('text');
+
+
+} 
+
+
+Close.onclick =function x( ){
+    list.classList.remove('text');  
+    
+    } 
+    
+
+    // iin this function we addd cllass called outfix and we get them in the css b
+    
+let box = document.getElementById('box');
+
+ box.addEventListener('click', function ( ){
+
+    box.classList.add('Outfix')
+    
+
+
+ }) 
+
+
+
+
+
+
+
+
+
+
+
+
